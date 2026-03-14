eb8bfd92dc6c54e2d54f56b14563aa4027a2b8fd
tovsa7 <96818847+tovsa7@users.noreply.github.com>
2026-03-14 21:04:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const C        = 'tovsa-v8';
 const C_FONTS  = 'tovsa-fonts-v1';
 const ICON_URL = './icon-192.png';
-const DB_NAME  = 'fpv_drun_v1';
-const DB_VER   = 3; // sync with main app (v3 adds 'meta' store)
+const DB_NAME  = 'tovsa_v1';
+const DB_VER   = 4; // v4 adds 'voice' store
 
 const PRECACHE = [
   './',
@@ -226,7 +226,7 @@ function _swOpenDB() {
     const req = indexedDB.open(DB_NAME, DB_VER);
     req.onupgradeneeded = e => {
       const db = e.target.result;
-      ['contacts','history','fav','files','keys','meta'].forEach(s => {
+      ['contacts','history','fav','files','voice','keys','meta','rooms','channels'].forEach(s => {
         if (!db.objectStoreNames.contains(s)) db.createObjectStore(s);
       });
     };

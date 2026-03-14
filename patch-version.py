2e64868ef607c06855f7ed05bad546f3f13997f2
tovsa7 <96818847+tovsa7@users.noreply.github.com>
2026-03-15 01:29:02 +0300

    Add files via upload

--- patch-version.py
+++ patch-version.py
@@ -4,21 +4,21 @@ semver = os.environ['SEMVER']
 pubkey = os.environ.get('PUBKEY', '')
 
 # tauri.conf.json
-with open('src-tauri/tauri.conf.json') as f:
+with open('src-tauri/tauri.conf.json', encoding='utf-8') as f:
     c = json.load(f)
 c['version'] = semver
 if pubkey and c.get('plugins', {}).get('updater'):
     c['plugins']['updater']['pubkey'] = pubkey
 elif not pubkey and c.get('plugins', {}).get('updater'):
     del c['plugins']['updater']
-with open('src-tauri/tauri.conf.json', 'w') as f:
+with open('src-tauri/tauri.conf.json', 'w', encoding='utf-8') as f:
     json.dump(c, f, indent=2)
 print('tauri.conf.json version =', semver)
 
 # Cargo.toml
-with open('src-tauri/Cargo.toml') as f:
+with open('src-tauri/Cargo.toml', encoding='utf-8') as f:
     t = f.read()
 t = re.sub(r'^version = "[^"]+"', 'version = "' + semver + '"', t, flags=re.MULTILINE)
-with open('src-tauri/Cargo.toml', 'w') as f:
+with open('src-tauri/Cargo.toml', 'w', encoding='utf-8') as f:
     f.write(t)
 print('Cargo.toml version =', semver)
